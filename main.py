32fa4b5f9f5ccef562db0c367c54ce40f79734a5
Ricardo Sousa <58951336+Ricardo-Sousa-hub@users.noreply.github.com>
2021-01-19 13:20:08 +0000

    Correção de tela de loggin

--- main.py
+++ main.py
@@ -16,28 +16,31 @@ while True:
                           "Criar nova conta - 2\n"
                           "Terminar - 0\n"
                           "Opcao: "))
-        if opcao == 1:
-            utilizador = str(input("Utilizador: "))
-            password = str(input("Password: "))
-
-            if utilizador == "Admin" and password == "admin":
-                MenuPrincipalAdmin()
-            else:
-                if utilizador in users:
-                    userpos = users.index(utilizador)
-                    passpos = palavrapasse.index(password)
-                    if userpos == passpos:
-                        print("Bem-vindo")
-                        MenuPrincipalUser(utilizador)
-                    else:
-                        print("Palavra passe incorreta ou utilziador nao existente")
-                else:
-                    print("Nome de utilziador incorreto ou nao existente")
-        elif opcao == 2:
-            Gestao_de_utilizadores.inserirUtilizadores()
-
-        elif opcao == 0:
-            break
+        while opcao < 0 or opcao > 2:
+            print("Por favor digite um valor entre 0 e 2")
+            opcao = int(input("Login - 1\n"
+                              "Criar nova conta - 2\n"
+                              "Terminar - 0\n"
+                              "Opcao: "))
     except Exception:
-        print("Digite um numero válido")
+        print("Por favor digite um numero valido")
+    if opcao == 1:
+        utilizador = str(input("Utilizador: "))
+        password = str(input("Password: "))
 
+        if utilizador == "Admin" and password == "admin":
+            MenuPrincipalAdmin()
+        else:
+            try:
+                userpos = users.index(utilizador)
+                passpos = palavrapasse.index(password)
+                if userpos == passpos:
+                    print("Bem-vindo")
+                    MenuPrincipalUser(utilizador)
+            except Exception:
+                print("Nome de utilziador/palavra passe incorreto(a) ou nao existente")
+    elif opcao == 2:
+        Gestao_de_utilizadores.inserirUtilizadores()
+
+    elif opcao == 0:
+        break
